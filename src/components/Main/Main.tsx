c1f9dbe4710968fc476aea41a4f44f4fc558e3b6
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-05-03 17:25:58 +0400

    new commit!

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,12 @@
+export default function Main() {
+  return (
+    <main className="pt-[21px] pr-[16px] pb-[16px] pl-[16px] w-[100%] max-h-[423px] bg-background3 mt-[24px]">
+      <section className="">
+        <div>
+          <h3 className="text-[18px] text-customColor3"> Character Length</h3>
+          <h3 className="text-[32px] text-customColor4">10</h3>
+        </div>
+      </section>
+    </main>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
         customColor: "#817D92",
         customColor2: "#817D92",
         customColor3: "#E6E5EA",
-        customColor4: "#9495A5",
+        customColor4: "#A4FFAF",
         customColor5: "#3A7CFD",
         customColor6: "#393A4B",
         customColor7: "#C8CBE7",
