46332fc7783f75a61fdf1bc710623cfe72dd1d6f
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-05-05 19:23:32 +0400

    new commit!

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,21 @@
-import { useCallback, useState } from "react";
 import copy from "/images/icon-copy.svg";
 
 export default function Header({ data, isActive, value }) {
-  const [info, setinfo] = useState("");
-  const getRandomCharacter = () => {
-    const randomIndex = Math.floor(Math.random() * value);
-    return data[0][randomIndex];
+  const getRandomCharacters = () => {
+    const randomCharacters = [];
+
+    for (let i = 0; i < value; i++) {
+      const randomIndex = Math.floor(Math.random() * data[0].length);
+      randomCharacters.push(data[0][randomIndex]);
+    }
+    const info = randomCharacters.filter((item) => item !== item.toLowerCase());
+    console.log();
+
+    return info.join("");
   };
+
+  console.log(getRandomCharacters());
+
   return (
     <header className="">
       <section className="flex justify-center  flex-col items-center cursor-pointer">
@@ -16,10 +25,11 @@ export default function Header({ data, isActive, value }) {
         <div className=" text-customColor3  flex justify-between items-center p-[16px] pointer-events-none w-[100%] h-[80px] bg-background3 text-Almost-White p-19 text-[32px] font-bold  ">
           <span className="opacity-30"> P4$5W0rD!</span>
           <p>
-            {getRandomCharacter()
+            {/* {getRandomCharacter()
               .split("")
-              .filter((item) => item !== item.toLowerCase())
-              .join("")}
+              .filter((item: string) => item !== item.toLowerCase())
+              .join("")} */}
+            {getRandomCharacters()}
           </p>
 
           <img src={copy} alt="copy_svg" />
