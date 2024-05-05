000a04baf522548735300df2c7e1edd6fc4b3076
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-05-05 16:07:25 +0400

    new commit!

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,12 @@
+import { useCallback, useState } from "react";
 import copy from "/images/icon-copy.svg";
 
 export default function Header({ data, isActive, value }) {
+  const [info, setinfo] = useState("");
+  const getRandomCharacter = () => {
+    const randomIndex = Math.floor(Math.random() * value);
+    return data[0][randomIndex];
+  };
   return (
     <header className="">
       <section className="flex justify-center  flex-col items-center cursor-pointer">
@@ -10,12 +16,10 @@ export default function Header({ data, isActive, value }) {
         <div className=" text-customColor3  flex justify-between items-center p-[16px] pointer-events-none w-[100%] h-[80px] bg-background3 text-Almost-White p-19 text-[32px] font-bold  ">
           <span className="opacity-30"> P4$5W0rD!</span>
           <p>
-            {isActive
-              ? data[0]
-                  .split("")
-                  .filter((item) => item !== item.toLowerCase())
-                  .join("")
-              : null}
+            {getRandomCharacter()
+              .split("")
+              .filter((item) => item !== item.toLowerCase())
+              .join("")}
           </p>
 
           <img src={copy} alt="copy_svg" />
