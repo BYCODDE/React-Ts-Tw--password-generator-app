1cd979b57f086b5dd7b4a049da97734ac9236b42
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-05-05 22:43:35 +0400

    new commit!

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -42,6 +42,21 @@ export default function Header({ data, isActive, value }) {
     }
     return numberCharacters;
   };
+  const getSymbolCharacter = () => {
+    const symbolRegex = /[!@#$%^&*()_+{}|:<>?`\-=[\];',./]/;
+    const symbolCharacters = [];
+    for (let i = 0; i < value; i++) {
+      const randomIndex = Math.floor(Math.random() * symbolRegex.source.length);
+      const randomChar = symbolRegex.source[randomIndex];
+      if (symbolRegex.test(randomChar)) {
+        symbolCharacters.push(randomChar);
+      } else {
+        i--;
+      }
+    }
+    return symbolCharacters;
+  };
+
   return (
     <header className="">
       <section className="flex justify-center  flex-col items-center cursor-pointer">
@@ -52,8 +67,8 @@ export default function Header({ data, isActive, value }) {
           <span className="opacity-30"> P4$5W0rD!</span>
           {/* <p>{getUpperCharacters()}</p> */}
           {/* <p>{getLowerCharacter()}</p> */}
-          <p>{getNumberCharacter()}</p>
-
+          {/* <p>{getNumberCharacter()}</p> */}
+          <p>{getSymbolCharacter()}</p>
           <img src={copy} alt="copy_svg" />
         </div>
       </section>
